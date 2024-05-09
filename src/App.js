1f60b5d5778edb541c8e1798801a19d6207f5691
Shenal Perera <pererashenal369@gmail.com>
2024-05-09 09:57:40 +0530

    01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Background from './Components/Background/Background';
 import Topbar from './Components/Topbar/Topbar';
 
 
