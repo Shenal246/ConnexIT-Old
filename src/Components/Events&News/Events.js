ee5d5805f79f6187dc59bb975e902476e74a3a89
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-05-09 17:22:12 +0530

    Event and News Page

--- src/Components/Events&News/Events.js
+++ src/Components/Events&News/Events.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import './Events.css';
+import card1 from '../../images/ConnexIT.png'; // Assuming card1 is the variable holding the image import
 
 const Events = () => {
-    return (<>
-        TEST123
-    </>);
+    return (
+        <div className="container">
+             {/* //first event row */}
+            <div className="row">  
+                <div className="col-md-6 col-lg-3 cd-1">
+                    <div className="card mb-3 card1">
+                        <img src={card1} className="card-img-top" alt="ConnexIT Logo" />
+                        <div className="card-body card1">
+                            <h5 className="card-title">Card title</h5>
+                            <a href="#" className="btn btn-primary button">See more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    );
 }
 
 export default Events;
