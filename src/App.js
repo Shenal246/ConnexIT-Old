5e20dfb33013ea6e1e8211b533cd38ac6d02d20e
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-05-09 09:54:55 +0530

    Footer Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <Navbar/>
+    <Footer/>
   );
 }
 
