aaaffd2e10aa87f3a6812657dc0391b3b9d3bc2b
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-05-10 13:22:48 +0530

    Events & news
    Updated some css parts

--- src/Components/Events&News/Events.js
+++ src/Components/Events&News/Events.js
@@ -11,7 +11,7 @@ const Events = () => {
                 <div className="col-lg-4 ">
                     <div className="card mb-3 card1 text-light position-relative crd rounded-5 ">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5 opacity-75" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
                             <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
@@ -31,7 +31,7 @@ const Events = () => {
                 <div className="col-lg-4 ">
                     <div className="card mb-3 card1 text-light position-relative crd rounded-5">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5 opacity-75" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
                             <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
@@ -49,7 +49,25 @@ const Events = () => {
                 <div className="col-lg-4 ">
                     <div className="card mb-3 card1 text-light position-relative crd rounded-5">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5 opacity-75" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            {/* Position the Link component at the center of the image */}
+                            <div className="centered">
+                            <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
+
+                            </div>
+                        </div>
+                        <div className="card-body card1 rounded-top-1 rounded-bottom-5">
+                            <div className="row">  
+                                <h5 className="card-title small-title">Card title.n publishing and graphic  is a ful content. Lorem ipsum may be used as a placeholder before the final co. Card title.n publishing and graphic  is a ful content. </h5> {/* Added small-title class */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-4 ">
+                    <div className="card mb-3 card1 text-light position-relative crd rounded-5 ">
+                        <div className="position-relative">
+                            <img src={card1} className="card-img-top image rounded-top-5 opacity-75" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
                             <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
@@ -67,25 +85,7 @@ const Events = () => {
                 <div className="col-lg-4 ">
                     <div className="card mb-3 card1 text-light position-relative crd rounded-5">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
-                            {/* Position the Link component at the center of the image */}
-                            <div className="centered">
-                            <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
-
-                            </div>
-                        </div>
-                        <div className="card-body card1 rounded-top-1 rounded-bottom-5">
-                            <div className="row">  
-                                <h5 className="card-title small-title">Card title.n publishing and graphic  is a ful content. Lorem ipsum may be used as a placeholder before the final co. Card title.n publishing and graphic  is a ful content. </h5> {/* Added small-title class */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-lg-4 ">
-                    <div className="card mb-3 card1 text-light position-relative crd rounded-5">
-                        <div className="position-relative">
-                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5 opacity-75" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
                             <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
