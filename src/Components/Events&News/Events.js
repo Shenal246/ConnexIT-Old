8bed2cb0ea2d881e8e171a0f34524ecda6e8e787
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-05-10 12:13:15 +0530

    Events CSS updated

--- src/Components/Events&News/Events.js
+++ src/Components/Events&News/Events.js
@@ -9,16 +9,36 @@ const Events = () => {
             {/* first event row */}
             <div className="row cards">  
                 <div className="col-lg-4 ">
-                    <div className="card mb-3 card1 text-light position-relative crd">
+                    <div className="card mb-3 card1 text-light position-relative crd rounded-5 ">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
                             <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
 
                             </div>
                         </div>
-                        <div className="card-body card1 rounded">
+                        <div className="card-body card1 rounded-top-1 rounded-bottom-5">
+                            <div className="row">  
+                                <h5 className="card-title small-title">Card title.n publishing and graphic  is a ful content. Lorem ipsum may be used as a placeholder before the final co. Card title.n publishing and graphic  is a ful content. </h5> {/* Added small-title class */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+                    
+                <div className="col-lg-4 ">
+                    <div className="card mb-3 card1 text-light position-relative crd rounded-5">
+                        <div className="position-relative">
+                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            {/* Position the Link component at the center of the image */}
+                            <div className="centered">
+                            <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
+
+                            </div>
+                        </div>
+                        <div className="card-body card1 rounded-top-1 rounded-bottom-5">
                             <div className="row">  
                                 <h5 className="card-title small-title">Card title.n publishing and graphic  is a ful content. Lorem ipsum may be used as a placeholder before the final co. Card title.n publishing and graphic  is a ful content. </h5> {/* Added small-title class */}
                             </div>
@@ -27,15 +47,16 @@ const Events = () => {
                 </div>
 
                 <div className="col-lg-4 ">
-                    <div className="card mb-3 card1 text-light position-relative crd">
+                    <div className="card mb-3 card1 text-light position-relative crd rounded-5">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
-                                <Link to="http://localhost:3000/" className="fa-solid fa-play playicon"></Link> {/* Changed span to Link and added z-index */}
+                            <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
+
                             </div>
                         </div>
-                        <div className="card-body card1 rounded">
+                        <div className="card-body card1 rounded-top-1 rounded-bottom-5">
                             <div className="row">  
                                 <h5 className="card-title small-title">Card title.n publishing and graphic  is a ful content. Lorem ipsum may be used as a placeholder before the final co. Card title.n publishing and graphic  is a ful content. </h5> {/* Added small-title class */}
                             </div>
@@ -44,15 +65,16 @@ const Events = () => {
                 </div>
 
                 <div className="col-lg-4 ">
-                    <div className="card mb-3 card1 text-light position-relative crd">
+                    <div className="card mb-3 card1 text-light position-relative crd rounded-5">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
-                                <Link to="http://localhost:3000/" className="fa-solid fa-play playicon"></Link> {/* Changed span to Link and added z-index */}
+                            <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
+
                             </div>
                         </div>
-                        <div className="card-body card1 rounded">
+                        <div className="card-body card1 rounded-top-1 rounded-bottom-5">
                             <div className="row">  
                                 <h5 className="card-title small-title">Card title.n publishing and graphic  is a ful content. Lorem ipsum may be used as a placeholder before the final co. Card title.n publishing and graphic  is a ful content. </h5> {/* Added small-title class */}
                             </div>
@@ -61,15 +83,16 @@ const Events = () => {
                 </div>
 
                 <div className="col-lg-4 ">
-                    <div className="card mb-3 card1 text-light position-relative crd">
+                    <div className="card mb-3 card1 text-light position-relative crd rounded-5">
                         <div className="position-relative">
-                            <img src={card1} className="card-img-top image" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
+                            <img src={card1} className="card-img-top image rounded-top-5" alt="ConnexIT Logo" style={{ width: '100%', height: 'auto' }} /> {/* Adjusted image size */}
                             {/* Position the Link component at the center of the image */}
                             <div className="centered">
-                                <Link to="http://localhost:3000/" className="fa-solid fa-play playicon"></Link> {/* Changed span to Link and added z-index */}
+                            <Link to="http://localhost:3000/" className="fa-solid fa-play playicon" ></Link>
+
                             </div>
                         </div>
-                        <div className="card-body card1 rounded">
+                        <div className="card-body card1 rounded-top-1 rounded-bottom-5">
                             <div className="row">  
                                 <h5 className="card-title small-title">Card title.n publishing and graphic  is a ful content. Lorem ipsum may be used as a placeholder before the final co. Card title.n publishing and graphic  is a ful content. </h5> {/* Added small-title class */}
                             </div>
