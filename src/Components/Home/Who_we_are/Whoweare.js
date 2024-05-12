b2558aff1d48cf054801098798c50f74c14fbe44
Shenal Perera <pererashenal369@gmail.com>
2024-05-12 19:21:49 +0530

    Whoweare changed

--- src/Components/Home/Who_we_are/Whoweare.js
+++ src/Components/Home/Who_we_are/Whoweare.js
@@ -1,5 +1,10 @@
 import './Whoweare.css'
 
+import Dis from '../../../images/tech/Distributer.png';
+import Con from '../../../images/tech/Consultancy.png';
+import Tra from '../../../images/tech/Training.png';
+import Tac from '../../../images/tech/TAC.png';
+
 function WhoweareSection() {
   return (
     <section id="about" className="section about">
@@ -13,8 +18,25 @@ function WhoweareSection() {
           </div>
 
           <div className="row gy-3">
-            <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-              {/* Put images here */}
+            <div className="col-lg-6 techcontainer" data-aos="fade-up" data-aos-delay="100">
+
+              <div className='row gy-3'>
+                <div className='col-lg-5 welogos'>
+                  <img src={Dis} className='techimage' />
+                </div>
+                <div className='col-lg-5 welogos'>
+                  <img src={Con} className='techimage'/>
+                </div>
+              </div>
+
+              <div className='row gy-3'>
+                <div className='col-lg-5 welogos'>
+                  <img src={Tra} className='techimage'/>
+                </div>
+                <div className='col-lg-5 welogos'>
+                  <img src={Tac} className='techimage'/>
+                </div>
+              </div>
             </div>
             <div className="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
 
