fea196f03c172fe0fa136728eb6166020ad015bd
Shenal Perera <pererashenal369@gmail.com>
2024-05-13 09:55:58 +0530

    who we are

--- src/Components/Home/Who_we_are/Whoweare.js
+++ src/Components/Home/Who_we_are/Whoweare.js
@@ -18,26 +18,34 @@ function WhoweareSection() {
           </div>
 
           <div className="row gy-3">
-            <div className="col-lg-6 techcontainer" data-aos="fade-up" data-aos-delay="100">
+            <div className="col-lg-6 techcontainer " data-aos="fade-up" data-aos-delay="100">
 
               <div className='row gy-3'>
-                <div className='col-lg-5 welogos'>
-                  <img src={Dis} className='techimage' />
+                <div className='col-2'></div>
+                <div className='col-lg-4 welogos'>
+                  <div className='row'><img src={Dis} className='techimage' /></div>
+                  <div className='row'><p className='techText'>Distribution</p></div>
                 </div>
-                <div className='col-lg-5 welogos'>
-                  <img src={Con} className='techimage'/>
+                <div className='col-lg-4 welogos'>
+                  <div className='row'><img src={Con} className='techimage' /></div>
+                  <div className='row'><p className='techText'>Consultancy</p></div>
                 </div>
               </div>
 
               <div className='row gy-3'>
-                <div className='col-lg-5 welogos'>
-                  <img src={Tra} className='techimage'/>
+                <div className='col-2'></div>
+                <div className='col-lg-4 welogos'>
+                  <div className='row'><img src={Tra} className='techimage' /></div>
+                  <div className='row'><p className='techText'>Training</p></div>
                 </div>
-                <div className='col-lg-5 welogos'>
-                  <img src={Tac} className='techimage'/>
+                <div className='col-lg-4 welogos'>
+                  <div className='row'><img src={Tac} className='techimage' /></div>
+                  <div className='row'><p className='techText'>TAC Support</p></div>
                 </div>
               </div>
             </div>
+
+
             <div className="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
 
               <div className="row" id='whText'>
@@ -55,6 +63,30 @@ function WhoweareSection() {
 
         </div>
       </div>
+
+      
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
     </section>
   );
 }
