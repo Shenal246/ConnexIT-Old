775d9f7f83ae21fff602245c4e4961087898a22d
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-05-14 13:16:43 +0530

    Latest part(home page)

--- src/Components/Home/LatestEvents/Latest.js
+++ src/Components/Home/LatestEvents/Latest.js
@@ -1,6 +1,7 @@
 import './Latest.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { BsArrowRight } from 'react-icons/bs'; // Importing Bootstrap icon
 
 import Latest1 from '../../../images/Latest/1.png';
 
@@ -14,14 +15,11 @@ const Latest = () => {
                     <div className="col-4" data-aos="fade-up" data-aos-delay="100"><p id='whoweareText'>Latest News</p></div>
                     <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
                 </div>
-                
-
 
                 <div className="row backgrnd">
-
                     <div className="col-md-6">
-                    {/* first image of the first colum */}
-                        <div class=" mb-2">
+                        {/* first image of the first column */}
+                        <div className="mb-2">
                             <Card className='cardContainer1'>
                                 <Card.Img variant="top" src={Latest1} className='image1' />
                                 <Card.Body className='cardBody'>
@@ -29,13 +27,12 @@ const Latest = () => {
                                 </Card.Body>
                             </Card>
                         </div>
-
                     </div>
-                    
-                    {/* First image of the Second colum */}
+
+                    {/* First image of the Second column */}
                     <div className="col-md-5">
                         <div className='row'>
-                            <div class=" mb-2">
+                            <div className="mb-2">
                                 <Card className='cardContainer2'>
                                     <Card.Img variant="top" src={Latest1} className='image2' />
                                     <Card.Body className='cardBody2'>
@@ -43,15 +40,13 @@ const Latest = () => {
                                     </Card.Body>
                                 </Card>
                             </div>
-
                         </div>
 
-
-                        <div class=" mb-2">
+                        <div className="mb-2">
                             <div className='row'>
                                 <div className="col-md-6">
-                                {/* First image of the Second colum second row */}
-                                    <div class=" mb-2">
+                                    {/* First image of the Second column second row */}
+                                    <div className="mb-2">
                                         <Card className='cardContainer2'>
                                             <Card.Img variant="top" src={Latest1} className='image2' />
                                             <Card.Body className='cardBody2'>
@@ -59,11 +54,10 @@ const Latest = () => {
                                             </Card.Body>
                                         </Card>
                                     </div>
-
                                 </div>
                                 <div className="col-md-6">
-                                {/* Second image of the Second colum second row */}
-                                    <div class=" mb-2">
+                                    {/* Second image of the Second column second row */}
+                                    <div className="mb-2">
                                         <Card className='cardContainer2'>
                                             <Card.Img variant="top" src={Latest1} className='image2' />
                                             <Card.Body className='cardBody2'>
@@ -73,30 +67,22 @@ const Latest = () => {
                                     </div>
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
 
-                    {/*see more button */}
+                    {/* See more button */}
                     <div className="col-md-1">
-
-                        <div class="navigation">
-                        <i class="fa-solid fa-chevrons-right arrow" ></i>
+                        <div className="navigation">
+                            <Button variant="" className='iconsss' style={{ backgroundColor: '#01066B' }}>
+                                <BsArrowRight size={30} />
+                            </Button>
                         </div>
-
                     </div>
 
                 </div>
-
-
-
-
             </div>
-
         </section>
     );
-
 }
 
 export default Latest;
