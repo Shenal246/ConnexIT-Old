d9ae5c9856b81ec6f390b9bbb4b11a7a7f6e2e67
Shenal Perera <pererashenal369@gmail.com>
2024-05-15 10:59:53 +0530

    Revert "Vendors update"
    This reverts commit eab7771106054ee9344a3a6b2392ccf42e6d1319.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-// import '@glightbox/core/dist/css/glightbox.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
